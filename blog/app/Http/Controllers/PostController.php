4b31b11f1ff78a50a1318695b47637e0e5e3c881
aliiahmadi <aliahmadi.github@gmail.com>
2023-02-27 20:43:31 +0330

    post controller

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -38,7 +38,36 @@ class PostController extends Controller
         //     echo $name . " " . $username;
         // }
 
+        // $resultOfJoin = DB::table("posts")->join("users", "users.id", "posts.user_id")->get();
 
+        // dd($resultOfJoin);
+
+        // // unions
+
+        // $first = DB::table("users")->whereNull("email");
+
+        // $second = DB::table("users")->whereNotNull("address")->union($first)->get();
+
+
+        // // order by
+
+
+        // $users = DB::table("users")->groupBy("country_code")->select("country_code", DB::raw("count * as total"))->get();
+
+
+        // $result = DB::table("users")->when(true, function ($query) {
+        //     return $query->where("id", ">", 120);
+        // })->get();
+
+        // this query will get first of all of them 
+
+        // i am here
+
+        // can you tell me something for create a new website
+        // i want to help you in this case you shold create you own wordpress application
+
+        $users = DB::table("posts")->select("content", "author as writer")->get();
+        dd($users);
     }
 
     /**
@@ -49,6 +78,17 @@ class PostController extends Controller
     public function create()
     {
         // return response()->file("theory-of-languages.pdf", ["Content-Type" => "pdf"]);
+
+        DB::transaction(function () {
+            DB::table("posts")->where("id", 100)->delete();
+
+            DB::table("posts")->insert([
+                "name" => "ali",
+                "family" => "ahmadi",
+                "country" => "iran",
+                "zip" => 100
+            ]);
+        });
     }
 
     /**
